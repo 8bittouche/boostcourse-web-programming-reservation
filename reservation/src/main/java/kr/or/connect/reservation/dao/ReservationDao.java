8563631e82d64d4371383a44febc9eaa5a081785
8bittouche <8bittouche@gmail.com>
2023-03-28 12:34:53 +0900

    modify db access (from jdbcTemplate to mybatis)

--- reservation/src/main/java/kr/or/connect/reservation/dao/ReservationDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/ReservationDao.java
@@ -1,87 +1,45 @@
 package kr.or.connect.reservation.dao;
 
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import kr.or.connect.reservation.dao.mapper.ReservationMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.reservation.dto.ReservationInfo;
 import kr.or.connect.reservation.dto.ReservationPrice;
 
-import static kr.or.connect.reservation.dao.ReservationDaoSqls.*;
-
 @Repository
+@RequiredArgsConstructor
 public class ReservationDao {
-	private NamedParameterJdbcTemplate namedParameterJdbc;
-	private JdbcTemplate jdbc;
-	private RowMapper<ReservationInfo> reservationInfoRowMapper = BeanPropertyRowMapper.newInstance(ReservationInfo.class);
-	private RowMapper<ReservationPrice> reservationPriceRowMapper = BeanPropertyRowMapper.newInstance(ReservationPrice.class);
-	
-	
-	public ReservationDao(DataSource dataSource) {
-        this.namedParameterJdbc = new NamedParameterJdbcTemplate(dataSource);
-        this.jdbc = new JdbcTemplate(dataSource);
-    }
-	
+
+	private final ReservationMapper reservationMapper;
+
 	public List<ReservationInfo> selectReservationInfo(String reservationEmail){
-		Map param = Collections.singletonMap("reservationEmail", reservationEmail);
-		return namedParameterJdbc.query(SELECT_RESERVATION_INFO, param, reservationInfoRowMapper);
+		return reservationMapper.selectReservationInfo(reservationEmail);
 	}
 	
 	public ReservationInfo selectReservationInfoById(int reservationInfoId){
-		Map param = Collections.singletonMap("reservationInfoId", reservationInfoId);
-		return namedParameterJdbc.queryForObject(SELECT_RESERVATION_INFO_BY_ID, param, reservationInfoRowMapper);
+		return reservationMapper.selectReservationInfoById(reservationInfoId);
 	}
 	
 	public List<ReservationPrice> selectReservationPrice(int reservationInfoId){
-		Map param = Collections.singletonMap("reservationInfoId", reservationInfoId);
-		return namedParameterJdbc.query(SELECT_RESERVATION_PRICE, param, reservationPriceRowMapper);
+		return reservationMapper.selectReservationPrice(reservationInfoId);
 	}
-	
-	
+
 	public int insertReservationInfoPrice(ReservationPrice reservationPrice) {
-		int reservationInfoId = reservationPrice.getReservationInfoId();
-				
-		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("reservationInfoId", reservationInfoId)
-													 .addValue("productPriceId", reservationPrice.getProductPriceId())
-													 .addValue("count", reservationPrice.getCount());
-				
-		return namedParameterJdbc.update(INSERT_RESERVATION_INFO_PRICE, namedParameters);
+		return reservationMapper.insertReservationInfoPrice(reservationPrice);
 	}
-	
-	
+
 	public int insertReservationInfo(ReservationInfo reservationInfo) {
-		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("productId", reservationInfo.getProductId())
-																	    .addValue("displayInfoId", reservationInfo.getDisplayInfoId())
-																	    .addValue("reservationName", reservationInfo.getReservationName())
-																 	    .addValue("reservationTelephone", reservationInfo.getReservationTelephone())
-																 	    .addValue("reservationEmail", reservationInfo.getReservationEmail())
-																	    .addValue("reservationDate", reservationInfo.getReservationDate())
-																	    .addValue("createDate", new Date())
-																	    .addValue("modifyDate", new Date());
-		
-		return namedParameterJdbc.update(INSERT_RESERVATION_INFO, namedParameters);
+		return reservationMapper.insertReservationInfo(reservationInfo);
 	}
-	
-	
+
 	public int updateCancelFlag(int reservationInfoId) {
-		Map param = Collections.singletonMap("reservationInfoId", reservationInfoId);
-		return namedParameterJdbc.update(UPDATE_CANCEL_FLAG, param);
+		return reservationMapper.updateCancelFlag(reservationInfoId);
 	}
-	
-	
+
 	public int selectLastInsertId() {
-		Integer identity = jdbc.queryForObject(SELECT_LAST_INSERT_ID, Integer.class);
-		return identity.intValue();
+		return reservationMapper.selectLastInsertId();
 	}
 }
