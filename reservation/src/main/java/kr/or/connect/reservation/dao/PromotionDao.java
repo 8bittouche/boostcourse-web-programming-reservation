7f45fbf5f37ecb369bbac3bbd78ac5598fcd79bc
8bittouche <8bittouche@gmail.com>
2023-03-29 12:18:57 +0900

    Revert "Revert "Merge pull request #9 from 8bittouche/develop""
    This reverts commit cf8fd831d68dac5dffff1eb7c5682a1e609a9478.

--- reservation/src/main/java/kr/or/connect/reservation/dao/PromotionDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/PromotionDao.java
@@ -2,19 +2,33 @@ package kr.or.connect.reservation.dao;
 
 import java.util.List;
 
-import kr.or.connect.reservation.dao.mapper.PromotionMapper;
-import lombok.RequiredArgsConstructor;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.reservation.dto.Promotion;
 
 @Repository
-@RequiredArgsConstructor
 public class PromotionDao {
-
-	private final PromotionMapper promotionMapper;
-
+	private static final String SELECT_PROMOTION_ALL = "SELECT promotion.id, "
+															+ "promotion.product_id, "
+															+ "file_info.save_file_name AS productImageUrl "
+													 + "FROM promotion "
+													 + "JOIN product_image ON product_image.type='th' AND promotion.product_id=product_image.product_id "
+													 + "JOIN file_info ON file_info.id=product_image.file_id";
+	
+	private JdbcTemplate jdbc;
+    private RowMapper<Promotion> promotionRowMapper = BeanPropertyRowMapper.newInstance(Promotion.class);
+    
+    
+    public PromotionDao(DataSource dataSource) {
+        this.jdbc = new JdbcTemplate(dataSource);
+    }
+    
     public List<Promotion> selectAll() {
-		return promotionMapper.selectAll();
+    	return jdbc.query(SELECT_PROMOTION_ALL, promotionRowMapper);
     }
 }
